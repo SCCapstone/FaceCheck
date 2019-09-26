2ec24ee487176a01af73b41719fe78c7efa4561f
Samuel Nichols <samuelnichols@Samuels-MacBook-Pro.local>
2019-09-26 16:56:56 -0400

    refactored stylesheet

---
+++ FaceCheckApp/src/assets/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textInput: {
+        height: 40,
+        width: '90%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 8
+    },
+    text: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: 40,
+    },
+    TouchableOpacity: {
+        margin: 5,
+    },
+    TouchableOpacityText: {
+        color: 'blue',
+        textAlign: 'center'
+    }
+  });
+
+export default styles;
