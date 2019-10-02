ad887f0e2fab4cf222a73c9c795cdd99e5e6bd9c
Samuel Nichols <samuelnichols@Samuels-MacBook-Pro.local>
2019-10-02 17:42:38 -0400

    more ui flow

--- FaceCheckApp/src/screens/StudentHomeScreen.js
+++ FaceCheckApp/src/screens/StudentHomeScreen.js
@@ -1,9 +1,40 @@
 import firebase from 'react-native-firebase';
 import React from 'react';
-import { FAB, Portal, Appbar, Card, Paragraph} from 'react-native-paper';
+import { FAB, Portal, Appbar, Card, Paragraph } from 'react-native-paper';
 import { Text, View, Button, ScrollView } from 'react-native';
 import mockClasses from 'FaceCheckApp/src/assets/mockClasses.json';
 import styles from 'FaceCheckApp/src/assets/styles';
+
+class ClassCards extends React.Component {
+  state = { currentUser: null, classes: mockClasses.classes } 
+  
+  componentDidMount() {
+      const { currentUser } = firebase.auth()
+      this.setState({ currentUser })
+  }
+
+  render() {
+      var { classes } = this.state
+      ClassCardsList = classes.map((currClass) => {
+          return(
+          <Card style={styles.card} key={currClass.classID} onPress = {() => {
+              this.props.navigation.navigate('ClassScreen', {currClass: JSON.stringify(currClass)})
+          }}>
+              <Card.Title title={currClass.name} />
+              <Card.Content>
+              <Paragraph>
+                  Teacher: {currClass.teacher}
+              </Paragraph>
+              </Card.Content>
+          </Card>
+          )})
+      return(
+          <View>
+              {ClassCardsList}
+          </View>
+      )
+  }
+}
 
 export default class StudentHomeScreen extends React.Component {
   state = { currentUser: null, open: false }
@@ -15,21 +46,6 @@ export default class StudentHomeScreen extends React.Component {
 
   render() {
     const { currentUser } = this.state
-    // Generating Cards from classes object
-    ClassCards = mockClasses.classes.map((currClass) => {
-      return(
-        <Card style={styles.card} key={currClass.classID} onPress = {() => {
-          this.props.navigation.navigate('ClassScreen', {currClass: JSON.stringify(currClass)})
-        }}>
-          <Card.Title title={currClass.name} />
-          <Card.Content>
-            <Paragraph>
-              Teacher: {currClass.teacher}
-            </Paragraph>
-          </Card.Content>
-        </Card>
-      )
-    })
     return (
       <View style = {styles.screen}>
         <Appbar.Header>
@@ -42,17 +58,14 @@ export default class StudentHomeScreen extends React.Component {
           />
         </Appbar.Header>
         <ScrollView>
-          {ClassCards}
+          <ClassCards navigation={this.props.navigation}/>
         </ScrollView>
         <Portal>
           <FAB.Group
             open={this.state.open}
-            icon={this.state.open ? 'today' : 'add'}
+            icon={this.state.open ? 'details' : 'class'}
             actions={[
-              { icon: 'add', onPress: () => console.log('Pressed add') },
-              { icon: 'star', label: 'Star', onPress: () => console.log('Pressed star')},
-              { icon: 'email', label: 'Email', onPress: () => console.log('Pressed email') },
-              { icon: 'bell', label: 'Remind', onPress: () => console.log('Pressed notifications') },
+              { icon: 'add', label: 'add a class', onPress: () => {ClassCards.forceUpdate()} }
             ]}
             onStateChange={({ open }) => this.setState({ open })}
             onPress={() => {
