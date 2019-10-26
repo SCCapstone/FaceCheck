2bc18e847e29af0e84725195582b93d5ee076d37
Jonathan Sharp <icannomnoms@gmail.com>
2019-10-26 15:56:02 -0400

    fixing code

--- FaceCheckApp/src/screens/TeacherHomeScreen.js
+++ FaceCheckApp/src/screens/TeacherHomeScreen.js
@@ -1,22 +1,60 @@
+import firebase from 'react-native-firebase';
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {FAB, Portal, Appbar} from 'react-native-paper';
+import {View, ScrollView, TouchableOpacity} from 'react-native';
+import ClassCards from 'FaceCheckApp/src/components/ClassCards.js';
+import styles from 'FaceCheckApp/src/assets/styles';
 
-const TeacherHomePageScreen = ({navigation}) => {
+export default class TeacherHomePageScreen extends React.Component {
   //TODO everything
+  state = {currentUser: null, open: false};
+
+  componentDidMount() {
+    const {currentUser} = firebase.auth();
+    this.setState({currentUser});
+  }
+  render() {
+    const {currentUser} = this.state;
   return (
-    <View>
-      <Text sytle={styles.otherStyle}>Teacher Home Page</Text>
-      <TouchableOpacity style={styles.textSytle}
-        onPress={() => navigation.navigate('TeacherClassView')}>
-        <Text style={styles.textSytle}>Go to Teacher Class View Page </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.textSytle}
-        onPress={() => navigation.navigate('TeacherViewStudent')}>
-        <Text style={styles.textSytle}>Go to Teacher View Student Info Page</Text>
-        </TouchableOpacity>
+    <View style={styles.screen}>
+        <Appbar.Header>
+          <Appbar.BackAction
+            title="Logout"
+            onPress={() => {
+              firebase.auth().signOut();
+            }}
+          />
+          <Appbar.Content title="Home" />
+        </Appbar.Header>
+        <ScrollView>
+          <ClassCards navigation={this.props.navigation} />
+        </ScrollView>
+        <FAB.Group
+          open={this.state.open}
+          icon={this.state.open ? 'details' : 'class'}
+          actions={[
+            {icon: 'add', label: 'add a class', onPress: () => {}},
+            {
+              icon: 'code',
+              label: 'qr generator',
+              onPress: () => {
+                this.props.navigation.navigate('QRGenerator');
+              },
+            },
+            {
+              icon: 'burst-mode',
+              label: 'qr scanner',
+              onPress: () => {
+                this.props.navigation.navigate('QRScanner');
+              },
+            },
+          ]}
+          onStateChange={({open}) => this.setState({open})}
+        />
       </View>
   );
 
+  };
 };
 
 const styles = StyleSheet.create({
