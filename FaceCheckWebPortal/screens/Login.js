e2f8ea324d918897c6f545443736159d2c462fe5
Ibrahim Salman <18r441m@gmail.com>
2019-10-29 16:53:37 -0400

    Fixed HomePage prop

--- FaceCheckWebPortal/screens/Login.js
+++ FaceCheckWebPortal/screens/Login.js
@@ -23,7 +23,7 @@ class Login extends React.Component {
 			if (user) {
 				this.props.getUser(user.uid)
 				if (this.props.user != null) {
-					this.props.navigation.navigate('Profile')
+					this.props.navigation.navigate('HomePage')
 				}
 			}
 		})
