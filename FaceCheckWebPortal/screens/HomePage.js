9953529062c52e4216b5b8ae1b51922987719994
Ibrahim Salman <18r441m@gmail.com>
2019-10-29 17:30:58 -0400

    added signout, needs love

--- FaceCheckWebPortal/screens/HomePage.js
+++ FaceCheckWebPortal/screens/HomePage.js
@@ -3,28 +3,60 @@ import { View, StyleSheet, Text, TouchableOpacity} from 'react-native'
 
 
 export default class HomePage extends React.Component {
+
     render() {
         return (
 
-            <View style = {styles.container}>
+            <View style = {styles.mainContainer}>
+                <View style = {styles.signout}>
+                <TouchableOpacity  style = {styles.sobutton}>
+                    <Text style = {styles.sobuttonText}>signout</Text>
+                </TouchableOpacity>
 
+                </View> 
+                <View style = {styles.container}>
                  <Text style = {styles.text}>HomePage</Text>
-            </View>
+                 </View>
+                 </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
 
+    mainContainer: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: '#ddd',
+        alignContent: 'center',
+        alignItems: 'center'
+ 
     },
     text: {
         fontSize: 20,
-        alignContent: 'center'
+    },
+
+    signout: {
+        flex: 1,
+        alignItems: "flex-end",
+        marginTop: 25,
+        marginRight: 25,  
+        maxHeight: 20,
+        marginBottom: 10
+    
+    },
+
+    sobutton: {
+        alignItems: 'flex-end',
+   
+    },
+    sobuttonText: {
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 
 
