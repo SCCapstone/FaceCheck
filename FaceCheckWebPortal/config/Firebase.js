31b532e559c15cfe0a4df8eb578a827f842d360b
Ibrahim Salman <18r441m@gmail.com>
2019-10-29 10:32:29 -0400

    init and firebase setup #30

---
+++ FaceCheckWebPortal/config/Firebase.js
@@ -0,0 +1,33 @@
+// Imports
+import firebase from 'firebase'
+import {
+	API_KEY,
+	AUTH_DOMAIN,
+	DATABASE_URL,
+	PROJECT_ID,
+	MESSAGE_SENDER_ID,
+	APP_ID
+} from 'react-native-dotenv'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	databaseURL: DATABASE_URL,
+	projectId: PROJECT_ID,
+	storageBucket: '',
+	messagingSenderId: MESSAGE_SENDER_ID,
+	appId: APP_ID
+}
+
+// Initialize Firebase
+let Firebase = firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.firestore()
+
+// Hide deprecated warnings
+db.settings({
+	timestampsInSnapshots: true
+})
+
+export default Firebase
