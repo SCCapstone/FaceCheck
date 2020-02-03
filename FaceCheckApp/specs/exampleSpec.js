4e8db5a5c889f5064805c9b430750421376ea7b3
Mike Miranda <mikemiranda1998@gmail.com>
2020-02-02 21:10:33 -0500

    added random account creation

--- FaceCheckApp/specs/exampleSpec.js
+++ FaceCheckApp/specs/exampleSpec.js
@@ -1,16 +1,20 @@
 export default function(spec) {
   spec.describe('Sign Up', function() {
     spec.it('Goes to SignUp and enters details', async function() {
+      var accountVar = Math.floor(Math.random() * Math.floor(999));
       await spec.exists('Scene.Image');
       await spec.press('Scene.SignUpButton');
-      await spec.fillIn('Scene.SignUpEmail', 'testemail@email.com');
-      await spec.fillIn('Scene.SignUpPassword', 'password');
+      await spec.fillIn(
+        'Scene.SignUpEmail',
+        `testemail${accountVar}@email.com`,
+      );
+      await spec.fillIn('Scene.SignUpPassword', `password${accountVar}`);
       await spec.press('Scene.handleSignUp');
 
       await spec.exists('Scene.Image');
-      await spec.fillIn('Scene.LoginEmail', 'testemail@gmail.com');
-      await spec.fillIn('Scene.LoginPassword', 'password');
-      await spec.press('Scene.LoginButton');
+      await spec.fillIn('Scene.LoginEmail', `testemail${accountVar}@email.com`);
+      await spec.fillIn('Scene.LoginPassword', `password${accountVar}`);
+      //await spec.press('Scene.LoginButton');
       //await spec.exists('Scene.ClassCard');
       //await spec.press('Scene.ClassCard');
     });
