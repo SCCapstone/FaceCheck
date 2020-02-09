7661c387f43d5d7fadc80f1561a29872ad88c8a4
Matthew Sharp <matthew.sharp7@gmail.com>
2020-02-09 15:46:12 -0500

    test for QRGenerator

--- FaceCheckApp/src/components/QRGenerator.js
+++ FaceCheckApp/src/components/QRGenerator.js
@@ -1,12 +1,13 @@
 import firebase from 'react-native-firebase';
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import {Surface} from 'react-native-paper';
+import {hook, wrap} from 'cavy';
 
 import styles from 'FaceCheckApp/src/assets/styles';
 
-export default class QRGenerator extends React.Component {
+class QRGenerator extends Component {
   constructor(props) {
     super(props);
     this.state = {init: true, seconds: 0, uid: null};
@@ -37,10 +38,12 @@ export default class QRGenerator extends React.Component {
       uid: this.state.uid,
       totpCode: Math.floor(Math.random() * Math.floor(999999)),
     });
+    const TestableFunctionComponent = wrap(QRCode);
     return (
       <View style={styles.centerItem}>
         <Surface style={styles.surface}>
-          <QRCode
+          <TestableFunctionComponent
+            ref={this.props.generateTestHook('Scene.QRCode')}
             style={{alignItems: 'center'}}
             value={qrData}
             size={150}
@@ -51,3 +54,5 @@ export default class QRGenerator extends React.Component {
     );
   }
 }
+
+export default hook(QRGenerator);
