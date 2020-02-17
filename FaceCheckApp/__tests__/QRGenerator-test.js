df1aaa3d409fdffd9a78d214d890c4c9df818b02
Jonathan Sharp <icannomnoms@gmail.com>
2020-02-16 22:34:35 -0500

    Working on more tests for QR page, not working at the moment

--- FaceCheckApp/__tests__/QRGenerator-test.js
+++ FaceCheckApp/__tests__/QRGenerator-test.js
@@ -6,12 +6,34 @@ import renderer from 'react-test-renderer';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+describe('Testing navigation', () => {
 
-// beforeEach(() => {
-//   wrapper = shallow(<QRGenerator />)
-// })
-test('check qr screen',()=>{
+  let wrapper = null
+  const spyNavigate = jest.fn()
+  const props = {
+    navigation: {
+      navigate: spyNavigate,
+      state: {}
+    }
+  }
+  const params = {
+    token: 'randomToken'
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<QRGenerator {...props} />)
+    wrapper.setState({ params: params })
+  })
+
+  test('Navigation Test', async () => {
+    await wrapper.instance()._goBack(params)
+    expect(spyNavigate).toHaveBeenCalled()
+  })
+})
+
+test('Test QR snapshot',()=>{
   let QRGeneratorData = renderer.create(<QRGenerator/>).toJSON;
   expect(QRGeneratorData).toMatchSnapshot();
 })
+
