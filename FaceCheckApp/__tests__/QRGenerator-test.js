546c7a42c0d734a24f036102291a828c05703762
Jonathan Sharp <icannomnoms@gmail.com>
2020-02-17 12:30:29 -0500

    removed non working tests

--- FaceCheckApp/__tests__/QRGenerator-test.js
+++ FaceCheckApp/__tests__/QRGenerator-test.js
@@ -9,31 +9,6 @@ import QRCode from 'react-native-qrcode-svg';
 
 configure({ adapter: new Adapter() });
 
-// describe('Testing navigation', () => {
-
-//   let wrapper = null
-//   const spyNavigate = jest.fn()
-//   const props = {
-//     navigation: {
-//       navigate: spyNavigate,
-//       state: {}
-//     }
-//   }
-//   const params = {
-//     token: 'randomToken'
-//   }
-
-//   beforeEach(() => {
-//     wrapper = shallow(<QRGenerator {...props} />)
-//     wrapper.setState({ params: params })
-//   })
-
-//   test('Navigation Test', async () => {
-//     await wrapper.instance()._goBack(params)
-//     expect(spyNavigate).toHaveBeenCalled()
-//   })
-// })
-
 test('Test QR snapshot',()=>{
   let QRGeneratorData = renderer.create(<QRGenerator/>).toJSON;
   expect(QRGeneratorData).toMatchSnapshot();
@@ -54,15 +29,3 @@ test('asymmetric matcher', () => {
   const received = {a: _in => _in};
   expect(received).toEqual(expected);
 })
-
-test('Renders correct QR code',() => {
-  const qrData = JSON.stringify({
-    uid: 123,
-    totpCode: Math.floor(Math.random() * Math.floor(999999)),
-  });
-
-  const wrapper = shallow(<QRCode value={qrData}/>);
-
-  return expect(wrapper.find('.value')).toEqual({qrData});
-})
-
