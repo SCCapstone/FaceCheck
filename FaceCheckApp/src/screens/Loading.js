1a811ae772395adba5c13c3da9779ec730def728
Samuel Nichols <samnichols314@gmail.com>
2020-02-29 16:06:44 -0500

    might undo

--- FaceCheckApp/src/screens/Loading.js
+++ FaceCheckApp/src/screens/Loading.js
@@ -2,6 +2,7 @@ import firebase from 'react-native-firebase';
 import React from 'react';
 import {View, Text, ActivityIndicator} from 'react-native';
 import {StackActions} from 'react-navigation';
+import firestore from 'react-native-firebase/firestore';
 
 import styles from 'FaceCheckApp/src/assets/styles';
 
@@ -11,9 +12,22 @@ export default class Loading extends React.Component {
       // !! Loading is used to set initial navication
       // TODO: Add future check on logged in user to determin teacher or student
       // login flow
-      console.log(user);
       if (user) {
-        this.props.navigation.navigate('StudentHome');
+        firestore()
+          .collection('users')
+          .document(user.uid)
+          .get()
+          .then(doc => {
+            const userType = doc.data().userType;
+            switch (userType) {
+              case 'Student':
+                this.props.navigation.navigate('StudentHome');
+              case 'Teacher':
+                this.props.navigation.navigate('TeacherHome');
+              default:
+                console.log('Unknown User Type. This is a bug, please report!');
+            }
+          });
       } else {
         StackActions.reset({
           index: 0,
