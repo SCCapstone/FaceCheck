8e2381d10b78b5bafcdc97ffa0a2bc913ff3d49a
Samuel Nichols <samnichols314@gmail.com>
2020-02-29 16:42:35 -0500

    teacher vs student nav added

--- FaceCheckApp/src/screens/Loading.js
+++ FaceCheckApp/src/screens/Loading.js
@@ -2,7 +2,7 @@ import firebase from 'react-native-firebase';
 import React from 'react';
 import {View, Text, ActivityIndicator} from 'react-native';
 import {StackActions} from 'react-navigation';
-import firestore from 'react-native-firebase/firestore';
+const db = firebase.firestore();
 
 import styles from 'FaceCheckApp/src/assets/styles';
 
@@ -13,21 +13,37 @@ export default class Loading extends React.Component {
       // TODO: Add future check on logged in user to determin teacher or student
       // login flow
       if (user) {
-        firestore()
+        firebase
+          .firestore()
           .collection('users')
-          .document(user.uid)
+          .doc(user.uid)
           .get()
           .then(doc => {
             const userType = doc.data().userType;
-            switch (userType) {
-              case 'Student':
-                this.props.navigation.navigate('StudentHome');
-              case 'Teacher':
-                this.props.navigation.navigate('TeacherHome');
-              default:
-                console.log('Unknown User Type. This is a bug, please report!');
+            if (userType == 'Student') {
+              this.props.navigation.navigate('StudentHome');
+            } else if (userType == 'Teacher') {
+              this.props.navigation.navigate('TeacherHome');
+            } else {
+              console.log(
+                'Unknown User Type. This is a bug and should be reported!',
+              );
             }
           });
+        // db.collection('users')
+        //   .document(user.uid)
+        //   .get()
+        //   .then(doc => {
+        //     const userType = doc.data().userType;
+        //     switch (userType) {
+        //       case 'Student':
+        //         this.props.navigation.navigate('StudentHome');
+        //       case 'Teacher':
+        //         this.props.navigation.navigate('TeacherHome');
+        //       default:
+        //         console.log('Unknown User Type. This is a bug, please report!');
+        //     }
+        //   });
       } else {
         StackActions.reset({
           index: 0,
